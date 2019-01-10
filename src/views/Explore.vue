7c418a4968cfa71006784df1d8279df7120dabe1
sunguide <sunguide@qq.com>
2019-01-11 01:02:52 +0800

    测试微信分享

--- src/views/Explore.vue
+++ src/views/Explore.vue
@@ -120,10 +120,11 @@
     <nest-nav page="explore"></nest-nav>
   </div>
 </template>
-
+<!--<script type="text/javascript" src="//res.wx.qq.com/open/js/jweixin-1.2.0.js"></script>-->
 <script>
   import DICT, {getSelecteds} from '../configs/DICT';
-
+  import Share from '../utils/Share';
+  Share.weixin("我的鸟窝，您的专业租售房服务平台！", "即可加入，尽享菲律宾投资生活。", window.location.href, "https://api.dev.ohmynest.com/storage/uploads/images/avatars/201901/05//1_1546682838_xwnEaadTbl.jpg");
   export default {
     name: 'Explore',
     data() {
