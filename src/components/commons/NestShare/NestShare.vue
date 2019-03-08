d0a9f7ecae9e1c798bf3d5375ac6e6bad1639bb3
sunguide <sunguide@qq.com>
2019-03-09 04:23:55 +0800

    分享测试

--- src/components/commons/NestShare/NestShare.vue
+++ src/components/commons/NestShare/NestShare.vue
@@ -46,24 +46,6 @@
   </div>
 </template>
 <script>
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '2238721389736235',
-      xfbml      : true,
-      version    : 'v3.2'
-    });
-    FB.AppEvents.logPageView();
-  };
-
-  (function(d, s, id){
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-</script>
-<script>
   export default {
     name: "NestShare",
     props: {
@@ -71,6 +53,24 @@
         type: Boolean,
         default: false
       }
+    },
+    created(){
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '2238721389736235',
+          xfbml      : true,
+          version    : 'v3.2'
+        });
+        FB.AppEvents.logPageView();
+      };
+
+      (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
     },
     methods: {
       facebook() {
