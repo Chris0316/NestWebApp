ee6c2c3642238a54ed07a7183ea9a0a7d0fdace5
Caldwell <caldwell@maxoof.net>
2019-03-21 14:50:09 +0800

    资讯详情页开发

--- src/views/article/Details.vue
+++ src/views/article/Details.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="article">
+    <div class="download">
+      <div class="logo"></div>
+      <div class="text">
+        <div class="main">鸟窝APP</div>
+        <div class="sub">找寻温馨小窝</div>
+      </div>
+      <div class="download-btn">立即打开</div>
+    </div>
     <div class="header border-bottom">
       <div class="back" @click="$router.go(-1);"></div>
       <div class="share"></div>
@@ -71,6 +79,15 @@
       background: url('../../assets/images/return-icon.png') no-repeat left center;
       background-size: .42rem .32rem;
     }
+    .share {
+      position: absolute;
+      top: 0;
+      right: .28rem;
+      width: .9rem;
+      height: 100%;
+      background: url('../../assets/images/icon_share_black.png') no-repeat right center;
+      background-size: .3rem .3rem;
+    }
     .app-body {
       flex: 1;
       overflow: hidden;
@@ -85,7 +102,47 @@
     }
     .banner {
       height: 2rem;
-      background: #0f9183;
+      background: url('../../assets/images/banner_article.png') no-repeat;
+      background-size: 100% 100%;
+    }
+    .download {
+      position: relative;
+      padding: 0 .28rem;
+      display: flex;
+      align-items: center;
+      height: 1rem;
+      .logo {
+        margin-right: .4rem;
+        width: .66rem;
+        height: .66rem;
+        background: url('../../assets/images/logo_download.png') no-repeat;
+        background-size: 100% 100%;
+      }
+      .text {
+        .main {
+          font-size: .3rem;
+          color: #333;
+          font-weight: bold;
+        }
+        .sub {
+          margin-top: .1rem;
+          font-size: .22rem;
+          color: #b3b3b3;
+        }
+      }
+      .download-btn {
+        position: absolute;
+        top: .22rem;
+        right: .28rem;
+        width: 1.6rem;
+        height: .56rem;
+        line-height: .56rem;
+        text-align: center;
+        font-size: .28rem;
+        color: #fff;
+        background-color: #0f9183;
+        border-radius: .1rem;
+      }
     }
   }
 </style>
