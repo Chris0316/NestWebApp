996c676b41de9bbb6e8b94c8cd9186ad3d3aabe4
chriszhou <chris23jordan@163.com>
2019-03-24 21:04:52 +0800

    1. 本地开发取消https请求限制 2. 兼容iphonex刘海屏样式

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -22,7 +22,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
 
   // these devServer options should be customized in /config/index.js
   devServer: {
-    https: true,
+    https: false,
     clientLogLevel: 'warning',
     historyApiFallback: {
       rewrites: [
